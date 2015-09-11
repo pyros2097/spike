208e00d0d8a2808b4cdbb56ea1fa00120359cd3b
Peter John <peter@playlyfe.com>
2015-09-11 15:41:04 +0530

    initial commit

---
+++ version.go
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright 2015 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package gdx
+
+/** The version of gdx upstream 0be0b8125e79d6e5b84ced4415c5b44ca20247f5
+ *
+ * @author pyros2097 */
+const (
+	/** the current version of gdx as a String in the major.minor.revision format **/
+	VERSION = "0.0.0"
+)
